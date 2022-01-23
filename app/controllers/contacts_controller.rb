5bfc3259659e9b4499566131db7157c9cf1b38d7
Higor <hhigorsilvachaves@gmail.com>
2022-01-23 15:50:20 -0300

    Add feature to create a contact

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,25 +1,38 @@
 class ContactsController < ApplicationController
-  before_action :authenticate_user!
+	before_action :authenticate_user!
 
-  def index
-    @contacts = User.find(current_user[:id]).contacts
-  end
+	def index
+		@contacts = User.find(current_user[:id]).contacts
+	end
 
-  def show
-  end
+	def show
+	end
 
-  def new
-  end
+	def new
+		@current_contact = Contact.new
+	end
 
-  def create
-  end
+	def create
+		@current_contact = Contact.new(contact_params)
+		@current_contact[:user_id] = current_user[:id]
 
-  def update
-  end
+		if @current_contact.save
+			redirect_to contact_path(@current_contact)
+		end
+	end
 
-  def edit
-  end
+	def update
+	end
 
-  def destroy
-  end
+	def edit
+	end
+
+	def destroy
+	end
+
+	private
+	
+	def contact_params
+		params.require(:contact).permit(:first_name, :last_name, :email, :telegram_profile, :phone_number, :gender)
+	end
 end
