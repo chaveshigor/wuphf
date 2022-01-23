ed495c6222d499b5378d82014779984853563bdb
Higor <hhigorsilvachaves@gmail.com>
2022-01-23 00:00:17 -0300

    Add contact model

---
+++ db/migrate/20220123025707_create_contacts.rb
@@ -0,0 +1,15 @@
+class CreateContacts < ActiveRecord::Migration[6.1]
+  def change
+    create_table :contacts do |t|
+      t.string :first_name
+      t.string :last_name
+      t.string :email
+      t.string :phone_number
+      t.string :telegram_profile
+      t.string :gender
+      t.references :user, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
