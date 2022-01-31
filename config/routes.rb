02e6da17bbdf6bc1a74cb38190b65e20471743be
Higor <hhigorsilvachaves@gmail.com>
2022-01-31 16:19:49 -0300

    Add resources routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,8 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  get 'messages/index'
-  get 'messages/show'
-  get 'messages/send_message'
   resources :contacts
+  resources :messages, only: %i[index show new create]
 
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
