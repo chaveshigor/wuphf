204d6e9ca8dbde7f3ea15a628e2b45bf91e9ce34
Higor <hhigorsilvachaves@gmail.com>
2022-02-02 08:42:30 -0300

    Add group controller and group views

---
+++ app/controllers/groups_controller.rb
@@ -0,0 +1,28 @@
+class GroupsController < ApplicationController
+  def index
+  end
+
+  def show
+  end
+
+  def new
+  end
+  
+  def create
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+  private
+
+  def group_params
+    params.require(:group).permit(:name, :description)
+  end
+end
