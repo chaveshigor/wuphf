20ddbc21e832937d46de3ec7e8acd3ddd1463ae6
Higor <hhigorsilvachaves@gmail.com>
2022-02-04 19:24:27 -0300

    Add service to add contacts to group

---
+++ app/services/add_users_to_group.rb
@@ -0,0 +1,21 @@
+# frozen_string_literal: true
+
+class AddUsersToGroup < ApplicationService
+  def initialize(group_id, contact_ids)
+    @group_id = group_id
+    @contact_ids = contact_ids
+  end
+
+  def call
+    add_users
+  end
+
+  private
+
+  attr_reader :group_id, :contact_ids
+
+  def add_users
+    group = Group.find(group_id)
+    group.contact_ids += contact_ids
+  end
+end
