8aa9e5a02b1bc96714487872b86c4c0994546a19
Higor <hhigorsilvachaves@gmail.com>
2022-02-04 19:38:09 -0300

    Add action to create group

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,28 +1,39 @@
-class GroupsController < ApplicationController
-  def index
-  end
+# frozen_string_literal: true
 
-  def show
-  end
+class GroupsController < ApplicationController
+  before_action :authenticate_user!
+  def index; end
+
+  def show; end
 
   def new
+    @new_group = Group.new
+    @user_contacts = Contact.where({ user_id: current_user.id }).map { |contact| [contact.first_name, contact.id] }
   end
-  
+
   def create
+    new_group = Group.new(group_params)
+    new_group.user_id = current_user.id
+    new_group.save
+
+    # members = ContactGroup.create({ group_id: group_id, contact_id: 1 })
+    puts group_members[:contact_ids].inspect, 'kkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkk'
+    AddUsersToGroup.call(new_group.id, group_members[:contact_ids])
   end
 
-  def edit
-  end
+  def edit; end
 
-  def update
-  end
+  def update; end
 
-  def destroy
-  end
+  def destroy; end
 
   private
 
   def group_params
     params.require(:group).permit(:name, :description)
+  end
+
+  def group_members
+    params.require(:group).permit(contact_ids: [])
   end
 end
