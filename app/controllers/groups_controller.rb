8016e2fc23cc2d5a4ce3a567ec05925e5c568c76
Higor <hhigorsilvachaves@gmail.com>
2022-02-13 20:18:02 -0300

    Add action to update group

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -2,6 +2,7 @@
 
 class GroupsController < ApplicationController
   before_action :authenticate_user!
+  skip_before_action :verify_authenticity_token
   def index
     @groups = Group.all
   end
@@ -12,7 +13,8 @@ class GroupsController < ApplicationController
 
   def new
     @new_group = Group.new
-    @user_contacts = Contact.where({ user_id: current_user.id }).map { |contact| [contact.first_name, contact.id] }
+    @user_contacts = Contact.where({ user_id: current_user.id })
+    @user_contacts = contact_sanitize(@user_contacts)
   end
 
   def create
@@ -23,11 +25,25 @@ class GroupsController < ApplicationController
     AddUsersToGroup.call(new_group.id, group_members[:contact_ids])
   end
 
-  def edit; end
+  def edit
+    @group = Group.find(params[:id])
+    contacts_in_group = @group.contacts
+    user_contacts = User.find(current_user[:id]).contacts
+    @contacts_not_in_group = user_contacts - contacts_in_group
+    @contacts_not_in_group = contact_sanitize(@contacts_not_in_group)
+  end
 
-  def update; end
+  def update
+    group = Group.find(params[:id])
+    group.name = group_edit[:name]
+    group.contact_ids += group_edit[:new_contacts]
+    group.contact_ids -= group_edit[:contacts_to_remove]
+  end
 
-  def destroy; end
+  def destroy
+    group = Group.find(params[:id])
+    group.destroy
+  end
 
   private
 
@@ -37,5 +53,13 @@ class GroupsController < ApplicationController
 
   def group_members
     params.require(:group).permit(contact_ids: [])
+  end
+
+  def group_edit
+    params.require(:group).permit(:name, :description, contacts_to_remove: [], new_contacts: [])
+  end
+
+  def contact_sanitize(contacts)
+    contacts.map { |contact| ["#{contact.first_name} #{contact.last_name}", contact.id] }
   end
 end
