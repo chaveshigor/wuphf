c9b308084f60c792c1c249a5926bf7b9997d2f25
Higor <hhigorsilvachaves@gmail.com>
2022-02-13 21:38:25 -0300

    Add relation between user and group

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,7 @@
 class User < ApplicationRecord
   has_many :contacts
   has_many :messages
+  has_many :groups
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
