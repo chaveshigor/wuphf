0c26e40e2f2a279fd685ded5c94ac519aa5af6c4
Higor <hhigorsilvachaves@gmail.com>
2022-02-14 22:33:47 -0300

    Add method to get chatid

--- app/api/telegram.rb
+++ app/api/telegram.rb
@@ -1,19 +1,31 @@
 # frozen_string_literal: true
 
+require 'telegram/bot'
+
 # Class to store telegram variables and methods
-class Telegram
-  def initialize(profile, first_name, last_name)
-    @profile = profile
-    @first_name = first_name
-    @last_name = last_name
-    @full_name = "#{first_name} #{last_name}"
+class TelegramNotification
+  def initialize
+    @bot_token = ENV['TELEGRAM_TOKEN']
   end
 
   def send_message
     true
   end
 
-  private
+  def save_chat_id(message)
+    contact = Contact.find_by(email: message.text)
+    return false if contact.nil?
 
-  attr_reader :profile, :first_name, :last_name, :full_name
+    contact.telegram_profile = message.chat.id
+    contact.save
+  end
+
+  def authorize
+    Telegram::Bot::Client.run(@bot_token) do |bot|
+      bot.listen do |message|
+        bot.api.send_message(chat_id: message.chat.id, text: "Olá, #{message.from.first_name}. Parece que alguém quer te mandar uns Wuphfs.\nPara permitir, é só digitar seu email.") if message.text
+        bot.api.send_message(chat_id: message.chat.id, text: 'Deu tudo certo :)') if save_chat_id(message)
+      end
+    end
+  end
 end
