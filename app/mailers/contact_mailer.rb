ff930fa6979884a317a6bb6df688e39e8c5c7efa
Higor <hhigorsilvachaves@gmail.com>
2022-02-15 21:37:13 -0300

    Refactor, bug solved

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -1,9 +1,7 @@
 class ContactMailer < ApplicationMailer
-  default from: 'email@example.com'
-
   def notify_new_contact
     @new_contact = params[:new_contact]
     @telegram_chat_url = 'algum link aqui'
-    mail(to: @user.email, subject: 'Você foi WUPHFADO')
+    mail(to: @new_contact.email, subject: 'Você foi WUPHFADO')
   end
 end
