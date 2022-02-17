fe24e79bc3eaca8355422de349b39ef0053328df
Higor <hhigorsilvachaves@gmail.com>
2022-02-16 22:27:26 -0300

    Add service to wuphf contacts

---
+++ app/services/wuphf_contact.rb
@@ -0,0 +1,26 @@
+class WuphfContact < ApplicationService
+  def initialize(contacts, message)
+    @contacts = contacts
+    @message = message
+  end
+
+  def call
+    wuphf_contacts
+  end
+
+  private
+  attr_reader :contacts, :message
+
+  def wuphf_contacts
+    contacts.each do |contact|
+      email_sended = ContactMailer.with(contact: contact, message: message).send_mail_message if contact.email
+      telegram_sended = TelegramNotification.new.send_message(contact, message) if contact.telegram_profile
+
+      message_info = ContactMessage.find_by(contact_id: contact.id, message_id: message.id)
+      message_info.email_sended = email_sended
+      message_info.telegram_sended = telegram_sended
+      
+      message_info.save
+    end
+  end
+end
