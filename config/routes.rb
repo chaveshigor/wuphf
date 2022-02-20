1e1fc61d4024af9756214c3849a209627d66ac3f
Higor <hhigorsilvachaves@gmail.com>
2022-02-20 18:04:18 -0300

    Add root path

--- config/routes.rb
+++ config/routes.rb
@@ -9,5 +9,7 @@ Rails.application.routes.draw do
   resources :groups
 
   devise_for :users
+
+  root to: 'messages#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
