2a1e0cf8a70d6a820fa1ecdfceeb2cc02339f1fe
Higor <hhigorsilvachaves@gmail.com>
2022-02-20 17:19:07 -0300

    Remove authorize method

--- app/api/telegram.rb
+++ app/api/telegram.rb
@@ -21,23 +21,4 @@ class TelegramNotification
 
     success
   end
-
-  def save_chat_id(message)
-    contact = Contact.find_by(email: message.text)
-    return false if contact.nil?
-
-    contact.telegram_chat_id = message.chat.id
-    contact.save
-  end
-
-  def authorize
-    Telegram::Bot::Client.run(@bot_token) do |bot|
-      bot.listen do |message|
-        message_text = "Olá, #{message.from.first_name}. Parece que alguém quer te mandar uns Wuphfs.\nPara permitir, é só digitar seu email."
-
-        bot.api.send_message(chat_id: message.chat.id, text: message_text) if message.text
-        bot.api.send_message(chat_id: message.chat.id, text: 'Deu tudo certo :)') if save_chat_id(message)
-      end
-    end
-  end
 end
