16abd4de19b174fee2cb838fe949bcc901a5bb9a
Higor <hhigorsilvachaves@gmail.com>
2022-02-22 23:40:33 -0300

    Refactor, getting user from tests

--- spec/factories/contact_factory.rb
+++ spec/factories/contact_factory.rb
@@ -5,14 +5,7 @@ FactoryBot.define do
     first_name { 'joaozin' }
     last_name  { 'sertanejo' }
     phone_number { '12345678' }
-    email { 'joaozinsertanejo@gmail.com' }
-    telegram_chat_id { 'pokemonlover' }
+    email { "#{first_name}.#{last_name}@example.com" }
     gender { 'Homem' }
-
-    trait :with_user do
-      before(:create) do |contact|
-        contact.user = create(:user)
-      end
-    end
   end
 end
