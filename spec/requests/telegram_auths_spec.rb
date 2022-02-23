a40d1616982f914542693cdb8700a975fadeba13
Higor <hhigorsilvachaves@gmail.com>
2022-02-23 07:27:56 -0300

    Add telegram auth tests

--- spec/requests/telegram_auths_spec.rb
+++ spec/requests/telegram_auths_spec.rb
@@ -3,10 +3,47 @@
 require 'rails_helper'
 
 RSpec.describe 'TelegramAuths', type: :request do
+  let!(:user) { create(:user) }
+  let!(:contact) { create(:contact, user: user) }
+
   describe 'GET /create' do
-    it 'returns http success' do
-      get '/telegram_auths/create'
+    it 'auth user to receive telegram messages' do
+      post '/auth_contacts/telegram', params: {
+        chat_id: '1234',
+        email: contact.email
+      }
+
+      body = eval response.body
+
       expect(response).to have_http_status(:success)
+      expect(body[:status]).to eq(true)
+      expect(body[:result]).to eq('contact authorized')
+    end
+
+    it 'should not auth user to receive telegram messages because chat_id is invalid' do
+      post '/auth_contacts/telegram', params: {
+        chat_id: '12342d',
+        email: contact.email
+      }
+
+      body = eval response.body
+
+      expect(response).to have_http_status(:success)
+      expect(body[:status]).to eq(false)
+      expect(body[:result]).to eq('invalid chat id')
+    end
+
+    it 'should not auth user to receive telegram messages because email is not registered' do
+      post '/auth_contacts/telegram', params: {
+        chat_id: '1234',
+        email: 'rogerinho.do.inga@example.com'
+      }
+
+      body = eval response.body
+
+      expect(response).to have_http_status(:success)
+      expect(body[:status]).to eq(false)
+      expect(body[:result]).to eq('contact not founded')
     end
   end
 end
