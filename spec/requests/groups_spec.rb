1c7fec09e6500e881e09d72e20d10f7f38730292
Higor <hhigorsilvachaves@gmail.com>
2022-02-23 23:26:01 -0300

    Add group tests

--- spec/requests/groups_spec.rb
+++ spec/requests/groups_spec.rb
@@ -6,6 +6,7 @@ RSpec.describe 'Groups', type: :request do
   let!(:user) { create(:user) }
   let!(:contact1) { create(:contact, user: user) }
   let!(:contact2) { create(:contact, user: user, first_name: 'son', last_name: 'goku') }
+  let!(:contact3) { create(:contact, user: user, first_name: 'Itachi', last_name: 'Uchiha') }
   let!(:group) { create(:group, user: user, contacts: [contact1, contact2]) }
 
   before do
@@ -58,7 +59,64 @@ RSpec.describe 'Groups', type: :request do
 
   describe 'DELETE /destroy' do
     it 'destroy a group' do
-      expect { delete group_path(group), params: { id: group.id }}.to change { Group.count }.by(-1)
+      expect { delete group_path(group), params: { id: group.id } }.to change { Group.count }.by(-1)
+    end
+  end
+
+  describe 'PUT /update' do
+    it 'change group name' do
+      group_params = {
+        group: {
+          id: group.id,
+          name: 'Akatsuke',
+          new_contacts: [],
+          contacts_to_remove: []
+        }
+      }
+
+      put group_path(group), params: group_params
+
+      expect(response).to have_http_status(302)
+      expect(group.reload.name).to eq('Akatsuke')
+      expect(response).to redirect_to(group_path(group))
+    end
+
+    it 'change group name and add contacts to group' do
+      group_params = {
+        group: {
+          id: group.id,
+          name: 'Akatsuke',
+          new_contacts: [contact3.id],
+          contacts_to_remove: []
+        }
+      }
+      contacts_in_group = group.contacts.count
+
+      put group_path(group), params: group_params
+
+      expect(response).to have_http_status(302)
+      expect(group.reload.name).to eq('Akatsuke')
+      expect(group.reload.contacts.count).to eq(contacts_in_group + 1)
+      expect(group.reload.contacts.find { |c| c.id == contact3.id }).to eq(contact3)
+      expect(response).to redirect_to(group_path(group))
+    end
+
+    it 'change group name and remove contact from group' do
+      group_params = {
+        group: {
+          id: group.id,
+          name: 'Akatsuke',
+          new_contacts: [],
+          contacts_to_remove: [contact2.id]
+        }
+      }
+      contacts_in_group = group.contacts.count
+      put group_path(group), params: group_params
+
+      expect(response).to have_http_status(302)
+      expect(group.reload.name).to eq('Akatsuke')
+      expect(group.reload.contacts.find { |c| c.id == contact2.id }).to eq(nil)
+      expect(response).to redirect_to(group_path(group))
     end
   end
 end
