e0ebb046940c751e0237efdb7d24e43bec4d9fb5
Higor <hhigorsilvachaves@gmail.com>
2022-02-26 17:05:39 -0300

    Add method to get a string with all contacts from a message

--- app/helpers/messages_helper.rb
+++ app/helpers/messages_helper.rb
@@ -1,4 +1,11 @@
 # frozen_string_literal: true
 
 module MessagesHelper
+  def contacts_to_str(contacts)
+    contacts_str = ''
+    contacts.each do |c|
+      contacts_str += ", #{c.first_name.capitalize} #{c.last_name.capitalize}"
+    end
+    contacts_str[2..]
+  end
 end
